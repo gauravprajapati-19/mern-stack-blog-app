284c08b819e77d50c9878a4d2672037c673b4a4f
Gaurav Prajapati <gauravprajapati1903@gmail.com>
2025-05-07 23:11:03 +0530

    Fixed some issues...

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 // ✅ Updated CORS configuration
-const allowedOrigins = ['http://localhost:3000', 'https://nextechdev.site', 'http://nextechdev.site'];
+const allowedOrigins = ['http://localhost:3000', 'http://nextechdev.site', 'https://nextechdev.site'];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
