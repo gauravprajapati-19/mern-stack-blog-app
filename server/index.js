16f6b1f8805c4f25093be1345d49b99ccb19137a
Gaurav Prajapati <gauravprajapati1903@gmail.com>
2025-05-07 17:04:52 +0530

    Error resolved

--- server/index.js
+++ server/index.js
@@ -11,9 +11,7 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-<<<<<<< HEAD
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-=======
 
 // ✅ Updated CORS configuration
 const allowedOrigins = ['http://localhost:3000', 'http://3.6.69.181:3000', 'https://nextechdev.site'];
@@ -28,7 +26,6 @@ app.use(cors({
     credentials: true
 }));
 
->>>>>>> 78b83ca132d688c15ce4654961a793ff14681328
 app.use(upload());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
